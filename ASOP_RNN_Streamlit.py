bc63f3140f57646b1022cf3f3ff80d86b47df2ca
Dan Kim <Dan153TCIM@gmail.com>
2024-03-06 21:09:55 -0600

    Remove page title

--- ASOP_RNN_Streamlit.py
+++ ASOP_RNN_Streamlit.py
@@ -5,10 +5,6 @@ import altair as alt
 from tensorflow.keras.models import load_model
 import pandas as pd
 
-st.set_page_config(
-    page_title="ASOP Dreaming Model",
-)
-
 
 def pdf_transform(preds, temperature=1.0, column_name="Base"):
     preds = np.asarray(preds).astype("float64")
